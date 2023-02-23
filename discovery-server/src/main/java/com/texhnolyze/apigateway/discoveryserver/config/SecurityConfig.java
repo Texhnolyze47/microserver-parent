53b1cb4828bb7ee6dee2cb51625b8292347990a5
Texhnolyze47 <ichigobankai136@gmail.com>
2023-02-22 21:58:46 -0600

    add: integracion parcial de keycloak

---
+++ discovery-server/src/main/java/com/texhnolyze/apigateway/discoveryserver/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.texhnolyze.apigateway.discoveryserver.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Value("${eureka.username")
+    private String username;
+    @Value("${eureka.password")
+    private String password;
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username(username)
+                .password(password)
+                .authorities("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeHttpRequests((auth) -> auth
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return httpSecurity.build();
+    }
+}
